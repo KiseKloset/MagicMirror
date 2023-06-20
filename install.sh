1b99fe700b850ff44b6c04582fef5e970cae1c82
minhkhoi1026 <minhkhoi1026@gmail.com>
2023-06-20 21:40:13 +0700

    update requirement

---
+++ install.sh
@@ -0,0 +1,5 @@
+conda install pytorch torchvision torchaudio pytorch-cuda=11.6 -c pytorch -c nvidia
+pip install cupy-cuda11x
+pip install opencv-python
+pip install pytorch-fid
+pip install tqdm mediapipe
